1ac92c7cb402dc25c18e55c9c1aae1eadcda6c92
Alfarabia <97126798+Alfarabia@users.noreply.github.com>
2022-02-23 13:12:18 +0600

    Update gtr3.js

--- gtr3.js
+++ gtr3.js
@@ -5,3 +5,11 @@ menuBtn.addEventListener('click', function(){
     menuBtn.classList.toggle('active');
     menu.classList.toggle('active');
 })
+
+let alert = document.querySelector(".login-form .alert");
+let submit = document.querySelector(".login-form .submit");
+submit.addEventListener("click", function(){
+    alert.classList.remove("disp-none");
+    alert.classList.add("disp-block");
+})
+
